cbea0e212aded63a38426bf3f530ef357bd9aa56
bcca4706-hash <github.com/bcca4706-hash/Nexus.ai.git>
2026-07-17 20:33:49 +0300

    Enable all game scenes

--- src/main.js
+++ src/main.js
@@ -1,38 +1,59 @@
 import Phaser from "phaser";
+
+import SplashScene from "./scenes/SplashScene.js";
+import MenuScene from "./scenes/MenuScene.js";
+import WorldMapScene from "./scenes/WorldMapScene.js";
 import GameScene from "./scenes/GameScene.js";
+import LayerGameScene from "./scenes/LayerGameScene.js";
+import ShopScene from "./scenes/ShopScene.js";
+import ThemeScene from "./scenes/ThemeScene.js";
+import SettingsScene from "./scenes/SettingsScene.js";
+import ProfileScene from "./scenes/ProfileScene.js";
+import ProfileEditorScene from "./scenes/ProfileEditorScene.js";
+import AchievementScene from "./scenes/AchievementScene.js";
+import LevelEditorScene from "./scenes/LevelEditorScene.js";
 
-const config={
+const config = {
+  type: Phaser.AUTO,
 
-type:Phaser.AUTO,
+  parent: "game",
 
-parent:"game",
+  width: 720,
+  height: 1280,
 
-width:720,
+  backgroundColor: "#0f172a",
 
-height:1280,
+  scene: [
+    SplashScene,
+    MenuScene,
+    WorldMapScene,
+    GameScene,
+    LayerGameScene,
+    ShopScene,
+    ThemeScene,
+    SettingsScene,
+    ProfileScene,
+    ProfileEditorScene,
+    AchievementScene,
+    LevelEditorScene
+  ],
 
-backgroundColor:"#0f172a",
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  },
 
-scene:[
-GameScene
-],
+  render: {
+    pixelArt: false,
+    antialias: true
+  },
 
-scale:{
-
-mode:Phaser.Scale.FIT,
-
-autoCenter:Phaser.Scale.CENTER_BOTH
-
-},
-
-render:{
-
-pixelArt:false,
-
-antialias:true
-
-}
-
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: false
+    }
+  }
 };
 
 new Phaser.Game(config);
